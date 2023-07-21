af094ec713348e64ab6e48b5f766be0846ba510c
Nikita <nikita2013348@gmail.com>
2023-07-21 17:36:46 +0300

    create collectionView and customCell

--- MoviesApp/MoviesApp/ViewController.swift
+++ MoviesApp/MoviesApp/ViewController.swift
@@ -4,16 +4,59 @@
 //
 //  Created by Dubrouski Nikita on 21.07.23.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    var myCollectionView: UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let view = UIView()
+        view.backgroundColor = .white
+        
+        let mosaicLayout = MosaicLayout()
+        myCollectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: mosaicLayout)// Add minimum line spacing
+        
+        myCollectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
+        myCollectionView?.backgroundColor = UIColor.white
+        
+        myCollectionView?.dataSource = self
+        myCollectionView?.delegate = self
+        
+        view.addSubview(myCollectionView ?? UICollectionView())
+        self.view = view
+        
     }
-
-
+    
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50_000
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath)
+        myCell.backgroundColor = UIColor.random
+       
+        return myCell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+ 
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+       print("User tapped on item \(indexPath.row)")
+    }
+}
+
+extension UIColor {
+    static var random: UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+}
